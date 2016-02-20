92386ba296fc7e19b6f73c490274677199c0bbd2
unknown <ravi.abhinav3@gmail.com>
2016-02-20 20:45:35 +0530

    Final working App with LAT+LONG and Address decoded.

--- app/src/main/java/com/example/omsairam/gps/MainActivity.java
+++ app/src/main/java/com/example/omsairam/gps/MainActivity.java
@@ -6,36 +6,35 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
-
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private final String LOG_TAG = "Debug";
-    private TextView txtOutput1;
-    private TextView txtAdd1;
+    private TextView txtinfo;
+    private TextView txtAdd;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    String errorMessage="err";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +47,10 @@ public class MainActivity extends AppCompatActivity
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .build();
+                .addApi(AppIndex.API).build();
 
-        txtOutput1 = (TextView) findViewById(R.id.txtOutput);
-        txtAdd1=(TextView) findViewById(R.id.txtAdd);
+        txtinfo = (TextView) findViewById(R.id.location_lat_view);
+        txtAdd = (TextView) findViewById(R.id.location_address_view);
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -74,9 +73,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -102,6 +98,9 @@ public class MainActivity extends AppCompatActivity
         super.onStop();
     }
 
+
+
+
     @Override
     public void onConnected(Bundle bundle) {
         mLocationRequest = LocationRequest.create();
@@ -121,22 +120,80 @@ public class MainActivity extends AppCompatActivity
             //return;
             LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         }
-       /* LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);*/
-
     }
 
     @Override
     public void onConnectionSuspended(int i) {
         Toast.makeText(getApplicationContext(), "GoogleApiClient has been suspended", Toast.LENGTH_LONG).show();//not able to connect this time only.
-        Log.i(LOG_TAG,"GoogleApiClient has been suspended");
+        Log.i(LOG_TAG, "GoogleApiClient has been suspended");
 
     }
+
+
+
 
     @Override
     public void onLocationChanged(Location location) {
         Log.i(LOG_TAG, location.toString());
-        Toast.makeText(getApplicationContext(), "HI"+location.toString(), Toast.LENGTH_SHORT).show();
-        txtOutput1.setText("HI" + location.toString());
+        Toast.makeText(getApplicationContext(), "HI "+location.toString(), Toast.LENGTH_LONG).show();
+        txtinfo.setText("HI " + location.toString());
+
+        //We only start the service to fetch the address if GoogleApiClient is connected.
+        if (mGoogleApiClient.isConnected() && location != null) {
+
+
+            ////////////////////////////////////////////////////////
+
+            Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+            List<Address> addresses = null;
+
+
+            try {
+                // Using getFromLocation() returns an array of Addresses for the area immediately
+                // surrounding the given latitude and longitude. The results are a best guess and are
+                // not guaranteed to be accurate.
+                addresses = geocoder.getFromLocation(
+                        location.getLatitude(),
+                        location.getLongitude(),
+                        // In this sample, we get just a single address.
+                        5);
+            } catch (IOException ioException) {
+                // Catch network or other I/O problems.
+                errorMessage = getString(R.string.service_not_available);
+                Log.e(LOG_TAG, errorMessage, ioException);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                // Catch invalid latitude or longitude values.
+                errorMessage = getString(R.string.invalid_lat_long_used);
+                Log.e(LOG_TAG, errorMessage + ". " +
+                        "Lat = " + location.getLatitude() +
+                        ", Long = " + location.getLongitude(), illegalArgumentException);
+            }
+            ArrayList<String> addressFragments = new ArrayList<String>();
+            // Handle case where no address was found.
+            if (addresses == null || addresses.size()  == 0) {
+                if (errorMessage.isEmpty()) {
+                    errorMessage = getString(R.string.no_address_found);
+                    Log.e(LOG_TAG, errorMessage);
+                }
+
+            } else {
+                for(Address add: addresses) {
+                   // Address address = addresses.get(0);
+
+                    String s = add.getAddressLine(0)+","+add.getAddressLine(1)+","
+                            +add.getAddressLine(2)+","+add.getAddressLine(3);
+
+                        addressFragments.add(s);
+
+                    Log.i(LOG_TAG, getString(R.string.address_found));
+                }
+            }
+
+            txtAdd.setText(TextUtils.join(System.getProperty("line.separator"), addressFragments));
+            ////////////////////////////////////////////////////////////
+
+
+        }
 
         ////////////////////////////////////////////////////
        /* String cityName=null;
@@ -158,10 +215,9 @@ public class MainActivity extends AppCompatActivity
         txtAdd1.setText(s);*/
 
     }
-
-    @Override
+ @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
         Toast.makeText(getApplicationContext(), "GoogleApiClient connection has failed", Toast.LENGTH_LONG).show();//not able to connect this time only.
-        Log.i(LOG_TAG,"GoogleApiClient connection has failed");
+        Log.i(LOG_TAG, "GoogleApiClient connection has failed");
     }
 }
